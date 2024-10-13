c5dce2c268bbda704d5cab0aaffe0c226b1b8cd2
Bollaro Dylan <bollarodylan@gmail.com>
2024-10-13 23:52:28 +0200

    fix data from backend

--- src/app/home-details/[idOrSlug]/page.tsx
+++ src/app/home-details/[idOrSlug]/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { Card, CardContent } from '@/components/ui/card'
+import { imageLoader } from '@/lib/utils'
 import axios from 'axios'
 import Image from 'next/image'
 import { useEffect, useState } from 'react'
@@ -10,10 +11,10 @@ export default function Page({ params }: { params: { idOrSlug: string } }) {
 
     async function getUser() {
         const url =
-            process.env.NEXT_PUBLIC_API +
-            `/recipe/${params.idOrSlug}?addRecipeInformation=true`
+            process.env.NEXT_PUBLIC_API + `/home-details/${params.idOrSlug}`
         try {
             const response = await axios.get(url ?? '')
+            console.log(response)
             setFetchData(response.data)
         } catch (error) {
             console.error(error)
@@ -29,19 +30,18 @@ export default function Page({ params }: { params: { idOrSlug: string } }) {
             <Card className="p-3">
                 <h2 className="text-center text-2xl">{fetchData.title}</h2>
                 <CardContent>
-                    <Image
-                        src={fetchData?.image ?? ''}
-                        alt={fetchData.title ?? ''}
-                        width={160}
-                        height={160}
-                        className={
-                            'object-cover transition-all hover:scale-105 aspect-square rounded'
-                        }
-                    />
+                    {fetchData?.images?.map((img: string) => (
+                        <Image
+                            loader={imageLoader}
+                            src={img}
+                            alt={img}
+                            width={160}
+                            height={160}
+                            className={'object-cover aspect-square rounded'}
+                        />
+                    ))}
                 </CardContent>
-                <p className="p-3">
-                    {fetchData?.summary?.replace(/<[^>]*>/g, '')}
-                </p>
+                <p className="p-3">{fetchData?.description}</p>
             </Card>
         </div>
     )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { Card } from '@/components/ui/card'
+import { imageLoader } from '@/lib/utils'
 import axios from 'axios'
 import Image from 'next/image'
 import Link from 'next/link'
@@ -10,10 +11,10 @@ export default function Home() {
     const [fetchData, setFetchData] = useState([])
 
     async function getUser() {
-        const url = process.env.NEXT_PUBLIC_API + '/recipe/complexSearch'
+        const url = process.env.NEXT_PUBLIC_API + '/home-details'
         try {
             const response = await axios.get(url ?? '')
-            setFetchData(response.data.results)
+            setFetchData(response.data)
         } catch (error) {
             console.error(error)
         }
@@ -28,18 +29,22 @@ export default function Home() {
             <div className="py-3 grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-2">
                 {fetchData?.map((data: any) => {
                     return (
-                        <Link href={`/recipe/${data?.slug}`} key={data?.slug}>
+                        <Link
+                            href={`/home-details/${data?.slug}`}
+                            key={data?.slug}
+                        >
                             <Card className="max-w-52 min-h-full hover:scale-105 duration-75 p-0">
                                 <div className="p-3 flex flex-col items-center">
                                     <Image
+                                        loader={imageLoader}
                                         key={`image_${data?.slug}`}
-                                        src={data.image}
+                                        src={data.images[0]}
                                         alt={data.title}
                                         width={160}
                                         height={160}
                                         priority
                                         className={
-                                            'object-cover  aspect-square rounded'
+                                            'object-cover aspect-square rounded'
                                         }
                                     />
                                     <p className="px-3">{data.title}</p>
